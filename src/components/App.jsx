246900248a54b5865adddf4411bca956be462874
Vlad-Moskalenko <vlad.moskalenko1993@gmail.com>
2022-12-24 17:34:13 +0200

    refact code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,10 @@
-import { Section } from './Statistics/Section';
+import { useState, useEffect } from 'react';
 import { Statistics } from './Statistics/Statistics';
-import { FeedbackOptions } from './Statistics/FeedbackOptions';
-import { Notification } from './Statistics/Notification';
-import { useState } from 'react';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Section } from './Section/Section';
 
 export const App = () => {
+<<<<<<< Updated upstream
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
@@ -31,31 +31,74 @@ export const App = () => {
   const countPositiveFeedbackPercentage = () => {
     if (good) {
       return Math.floor((good / countTotalFeedback()) * 100);
+=======
+  const INITIAL_STATE = JSON.parse(localStorage.getItem('feedbackData')) ?? {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  const [feedbackData, setFeedbackData] = useState(INITIAL_STATE);
+
+  useEffect(
+    () => localStorage.setItem('feedbackData', JSON.stringify(feedbackData)),
+    [feedbackData]
+  );
+
+  const onLeaveFeedback = e => {
+    const feedbackOption = e.target.dataset.feedback;
+
+    setFeedbackData(prevState => ({
+      ...prevState,
+      [feedbackOption]: feedbackData[feedbackOption] + 1,
+    }));
+  };
+
+  const countTotalFeedback = () => {
+    return Object.values(feedbackData).reduce((a, b) => a + b, 0);
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    if (feedbackData.good) {
+      return Math.floor((feedbackData.good / countTotalFeedback()) * 100) + '%';
+>>>>>>> Stashed changes
     }
-    return 0;
+
+    return '-';
   };
 
   return (
-    <div className="app">
+    <main>
       <Section title="Please leave feedback">
         <FeedbackOptions
+<<<<<<< Updated upstream
           options={{ good, neutral, bad }}
           onLeaveFeedBack={e => onLeaveFeedBack(e)}
+=======
+          options={Object.keys(feedbackData)}
+          onLeaveFeedback={onLeaveFeedback}
+>>>>>>> Stashed changes
         />
       </Section>
       <Section title="Statistics">
         {!countTotalFeedback() ? (
-          <Notification message="There is no feedback" />
+          <h3>There is no feedback yet...</h3>
         ) : (
           <Statistics
+<<<<<<< Updated upstream
             good={good}
             neutral={neutral}
             bad={bad}
+=======
+            good={feedbackData.good}
+            neutral={feedbackData.neutral}
+            bad={feedbackData.bad}
+>>>>>>> Stashed changes
             total={countTotalFeedback()}
             positivePercentage={countPositiveFeedbackPercentage()}
           />
         )}
       </Section>
-    </div>
+    </main>
   );
 };
